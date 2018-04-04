8e77c08ecf7ffa7435a23fc435989719c736d905
yangchi <mywstszx@163.com>
2018-04-04 16:57:07 +0800

    新建一个页面

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,154 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(new MyApp());
+//void main() => runApp(new MyApp());
+void main() {
+  var x;
+
+  // 比较高阶函数
+  x = foo;
+  print(foo == x);
+
+  // 比较静态方法
+  x = SomeClass.bar;
+  print(SomeClass.bar == x);
+
+  // 比较实例方法
+  var v = new SomeClass();
+  var w = new SomeClass();
+  var y = w;
+  x = w.baz;
+  print(y.baz == x);
+  print(v.baz != w.baz);
+  runApp(new MyApp());
+}
+
+// 一个高阶函数
+foo() {}
+
+class SomeClass {
+  // 一个静态方法
+  static void bar() {}
+  // 一个实例方法
+  void baz() {}
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Welcome to Flutter',
-      theme: new ThemeData(
-        primaryColor: Colors.lightGreen
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+              child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Container(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: new Text(
+                  '我是第一行',
+                  style: new TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              new Text(
+                '我是第二行',
+                style: new TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
+          )),
+          new Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          new Text('41'),
+        ],
       ),
-      home: new RandomWords(),
+    );
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+      return new Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Icon(icon, color: color),
+          new Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: new Text(
+              label,
+              style: new TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    Widget buttonSection = new Container(
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          buildButtonColumn(Icons.call, '电话'),
+          buildButtonColumn(Icons.near_me, '导航'),
+          buildButtonColumn(Icons.share, '分享'),
+        ],
+      ),
+    );
+    Widget textSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Text(
+        '''
+        附件二减肥咖啡金额开积分卡积分卡分空间访客访客看附件,二会计法
+        副科级方可看附件二开发疯狂金风科技
+        ''',
+        softWrap: true,
+      ),
+    );
+
+    return new MaterialApp(
+      title: '这是flutter的demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('顶部湖泊'),
+        ),
+        body: new ListView(
+          children: <Widget>[
+            new Image.asset(
+              'images/lake.jpg',
+              width: 600.0,
+              height: 240.0,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
     );
   }
 }
+
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return new MaterialApp(
+//      title: 'Welcome to Flutter',
+//      theme: new ThemeData(primaryColor: Colors.lightGreen),
+//      home: new RandomWords(),
+//    );
+//  }
+//}
 
 class RandomWords extends StatefulWidget {
   @override
